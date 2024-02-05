e624cbf1600db59e769cc01bafaacf3110a2d9bf
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-02-05 10:18:44 +0100

    SS-9 Refactor Home screen to display user profiles using FlatList

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { FlatList, View } from 'react-native'
+import ProfilCard from '../components/ProfilCard'
+import { DATA } from '../data/usersData'
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    
+    return (
+        <View style={{ }}>
+            <FlatList style={''} data={DATA}
+                renderItem={({ item }) => <ProfilCard user={item} />}
+                keyExtractor={item => item.id} />
+        </View>
+    )
 }
